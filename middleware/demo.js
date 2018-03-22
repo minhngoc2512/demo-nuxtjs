7c78a9e2e9bcad238cb1a43803684ef278fc04d7
minhngoc2512 <minhngoc2512@yahoo.com>
2018-03-22 17:30:56 +0700

    update

--- middleware/demo.js
+++ middleware/demo.js
@@ -1,6 +1,10 @@
 
 export default function ({ store, redirect , route,req}) {
-  if(req.headers['user-agent']==='Googlebot/2.1 (+http://www.googlebot.com/bot.html)'){
-    store.commit('changeStatusSsr');
+  if(store.state.status_check === false) {
+    if (req.headers['user-agent'] === 'Googlebot/2.1 (+http://www.googlebot.com/bot.html)') {
+      store.commit('changeStatusSsr');
+    }
+    store.commit('changeStatusCheck');
   }
+
 }
--- store/index.js
+++ store/index.js
@@ -3,11 +3,15 @@ import Vuex from 'vuex'
 const createStore = () => {
   return new Vuex.Store({
     state: {
-      status_ssr:false
+      status_ssr:false,
+      status_check:false
     },
     mutations: {
       changeStatusSsr (state) {
         state.status_ssr=true;
+      },
+      changeStatusCheck(state){
+        state.status_check=true;
       }
     }
   })
